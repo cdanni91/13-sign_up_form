27cb88c623d27ae9f20ad050e9ef5cdf6860ce01
cdanni91 <cdanni91@gmail.com>
2025-05-14 14:01:14 -0300

    Finish the general layout and behaviour rules

---
+++ javascript.js
@@ -0,0 +1,24 @@
+function checkPasswords() {
+    const userPassword = document.querySelector("#password");
+    const userConfirmPassword = document.querySelector("#confirm-password");
+
+    let isMatch = false;
+    userPassword.value === userConfirmPassword.value ? isMatch = true : isMatch = false;
+    console.log(isMatch);
+
+    if (!isMatch) {
+        userPassword.style.outline = "2px solid red";
+        userConfirmPassword.style.outline = "2px solid red";
+    } else {
+        userPassword.style.outline = "2px solid green";
+        userConfirmPassword.style.outline = "2px solid green";
+    }
+}
+
+
+// Añadir evento blur a ambos inputs
+const passwordInput = document.querySelector("#password");
+const confirmPasswordInput = document.querySelector("#confirm-password");
+
+passwordInput.addEventListener("blur", checkPasswords);
+confirmPasswordInput.addEventListener("blur", checkPasswords);
